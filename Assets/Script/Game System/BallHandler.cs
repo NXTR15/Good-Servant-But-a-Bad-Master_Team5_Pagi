5228ab4f7345860d7ea299609269ebd7c545beb4
NXTR15 <abielheroes5@gmail.com>
2024-06-06 23:36:11 +0700

    Basketball launch

--- Assets/Script/Game System/BallHandler.cs
+++ Assets/Script/Game System/BallHandler.cs
@@ -1,37 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D currentBallRigidbody;
+    [SerializeField] private GameObject ballPrefab; 
+    [SerializeField] private float DelayDuration;
+    [SerializeField] private float RespawnDelay;
+
+    private Rigidbody2D pivot;
     private Camera m_Camera;
     private Vector3 mousePos;
+    private bool isDragging;
+    private Rigidbody2D currentBallRigidbody;
+    private SpringJoint2D currentBallSpringJoint;
     private void Start()
     {
         m_Camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        pivot = GameObject.FindGameObjectWithTag("PivotPoint").GetComponent<Rigidbody2D>();
+
+        SpawnNewBall();
     }
 
     private void Update()
     {
-        if(!Input.GetMouseButton(0) && currentBallRigidbody != null)
+        if(currentBallRigidbody == null) { return; }
+        if(pivot == null) { return; }
+
+        if (!Input.GetMouseButton(0))
         {
-            currentBallRigidbody.isKinematic = false;
+            if(isDragging)
+            {
+                StartCoroutine(LaunchBall());
+            }
+            isDragging = false;
+            
             return;
         }
 
-        if(currentBallRigidbody != null)
-        {
-            currentBallRigidbody.isKinematic = true;
-        }
+        isDragging = true;
+
+        currentBallRigidbody.isKinematic = true;
               
         mousePos = m_Camera.ScreenToWorldPoint(Input.mousePosition);
 
-        if(currentBallRigidbody != null)
-        {
-            currentBallRigidbody.position = mousePos;
-        }
-            
+        currentBallRigidbody.position = mousePos;                  
+    }
+
+    private void SpawnNewBall()
+    {
+        GameObject ballInstance = Instantiate(ballPrefab, pivot.position, Quaternion.identity);
+        currentBallRigidbody = ballInstance.GetComponent<Rigidbody2D>();
+        currentBallSpringJoint = ballInstance.GetComponent<SpringJoint2D>();
+
+        currentBallSpringJoint.connectedBody = pivot;
+    }
+
+    IEnumerator LaunchBall()
+    {
+        currentBallRigidbody.isKinematic = false;
+        currentBallRigidbody = null;
+
+        yield return new WaitForSeconds(DelayDuration);
+
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
+
+        yield return new WaitForSeconds(RespawnDelay);
+
+        SpawnNewBall();
     }
 }
